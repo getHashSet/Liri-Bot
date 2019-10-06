398f37790f90536aebd35168836fab42f3b1c5c7
Matthew <matthew_carpenter@icloud.com>
2019-10-06 12:31:27 -0700

    removed key filter, updated all spotify content. Partied like it was 1999.

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('keys have loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "52f7ac17f031451292dcf10964bd5a51", //process.env.SPOTIFY_ID,
+  secret: "314f7ff569cb40faa88dbce3a86e13a9" //process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -2,21 +2,118 @@
 ///////////////// NPM
 //////////////////////////////////////////////
 
-var axios = require("axios");
-var inquirer = require("inquirer");
-var moment = require("moment");
-var nodeSpotifyApi = require("node-spotify-api");
+let axios = require("axios");
+let fileSystem = require("fs");
+let inquirer = require("inquirer");
+let moment = require("moment");
+let Spotify = require('node-spotify-api');
 let keys = require("./keys.js");
 require("dotenv").config();
+
+//////////////////////////////////////////////
+///////////////// keys
+//////////////////////////////////////////////
+
+let spotify = new Spotify(keys.spotify);
+
+//////////////////////////////////////////////
+///////////////// inquirer
+//////////////////////////////////////////////
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "input",
+            message: "Select from the follwing options.",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
+        }
+    ])
+    .then(answers => {
+        switch (answers.input) {
+            case "concert-this":
+                concertThis();
+                break;
+            case "spotify-this-song":
+                spotifyThisSong();
+                break;
+            case "movie-this":
+                movieThis();
+                break;
+            case "do-what-it-says":
+                doWhatItSays()
+                break;
+            default:
+                console.log(`Something's gone wrong!`);
+                break;
+        }
+    });
+
 
 //////////////////////////////////////////////
 ///////////////// Functions
 //////////////////////////////////////////////
 
-var spotify = new Spotify(keys.spotify);
-console.log(spotify);
+const concertThis = () => {
+    console.log(`concertThis() fucntion called.`);
+};
+
+const spotifyThisSong = () => {
+    let userInputSong = "All the small things";
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "inputData",
+            message: "Name a song! *spelling matters*"
+        }
+    ]).then(songAnswer => {
+
+        // default song if input field was left blank.
+        songAnswer.inputData.length < 1 ?
+        userInputSong = "Ace of Base" :
+        userInputSong = songAnswer.inputData;
+
+        //spotifySearch();
+        spotifySearchFunction(userInputSong);
+    });
+};
+const movieThis = () => {
+    console.log(`movieThis() fucntion called.`);
+};
+const doWhatItSays = () => {
+    console.log(``);
+    console.log(`-------------------------------------`);
+    fileSystem.readFile("./random.txt", "UTF-8", (err, data) => {
+        let song = data;
+        console.log(song);
+        spotifySearchFunction("wild wild west");
+        console.log(`-------------------------------------`);
+    });
+};
+
+const spotifySearchFunction = (songInput) => {
+    //now that we have the song lets search it!
+    function ThisSong(_artist, _songName, _previewLink, _album) {
+        this.artist = _artist,
+            this.songName = _songName,
+            this.album = _album,
+            this.previewLink = _previewLink
+    };
+    spotify.search({
+        type: 'track', query: songInput
+    }).then(function (response) {
+        console.log(`------------------ START ------------------`);
+        let finalPreviewLink = "No preview link available";
+        let result = response.tracks.items[0];
+        result.preview_url === null ? finalPreviewLink = "No preview link available." : finalPreviewLink = result.preview_url;
+        let thisData = new ThisSong(result.artists[0].name, result.name, finalPreviewLink, result.album.name);
+        console.table(thisData);
+        console.log(`------------------ FIN ------------------`);
+    }).catch(function (err) {
+        console.log(err);
+    });
+};
 //////////////////////////////////////////////
 ///////////////// FIN
 //////////////////////////////////////////////
 
-console.log(`code ran`);
